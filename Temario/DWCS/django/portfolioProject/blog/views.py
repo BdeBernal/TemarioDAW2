3a1b71d06df2af20a9f8a537d0e73bc7a213b640
Bernal <=>
2024-11-15 12:51:36 +0100

    Blog cheto

--- Temario/DWCS/django/portfolioProject/blog/views.py
+++ Temario/DWCS/django/portfolioProject/blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Entrance
 # Create your views here.
 
 def all_blogs(request):
-    entrances = Entrance.objects.all()
+    entrances = Entrance.objects.order_by('-date')[:3] # Only to show the latest 3
     return render(request, 'entradas/all_blogs.html', {'entrances': entrances})
+
+def details(request, blog_id):
+    entrance = get_object_or_404(Entrance, pk=blog_id)
+    return render(request, 'entradas/details.html', {'entrance': entrance})
