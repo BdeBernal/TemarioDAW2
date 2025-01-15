6c0793a4a4c9603f12a4bae02586c7b69ad353dd
Bernal <=>
2025-01-15 11:09:10 +0100

    proyecto django

--- Temario/DWCS/django2/book_store/book_outlet/views.py
+++ Temario/DWCS/django2/book_store/book_outlet/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
 from .models import Book
 
 # Create your views here.
 def index(request):
     Books = Book.objects.all()
     return render(request, 'index.html', {'Books': Books})
+
+def detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'detail.html', {'book': book})
