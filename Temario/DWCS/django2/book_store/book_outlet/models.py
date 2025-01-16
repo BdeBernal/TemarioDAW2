4bb0f0f1f9e4aa3f872d04089e7020f3acffd4aa
Bernal <=>
2025-01-16 13:37:14 +0100

    django done

--- Temario/DWCS/django2/book_store/book_outlet/models.py
+++ Temario/DWCS/django2/book_store/book_outlet/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import Q
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 class Book(models.Model):
@@ -10,6 +12,10 @@ class Book(models.Model):
         )
     author = models.CharField(null=True, max_length=100)
     bestSelling = models.BooleanField(default=False)
+    slug = models.SlugField(default='', null=False, db_index=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[self.slug])
 
     def __str__(self):
-        return self.title
+        return f"{self.title}, {self.author}, {self.rating}, {self.bestSelling}, {self.slug}"
--- Temario/DWCS/django2/book_store/book_outlet/views.py
+++ Temario/DWCS/django2/book_store/book_outlet/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Book
+from django.db.models import Avg
 
 # Create your views here.
 def index(request):
-    Books = Book.objects.all()
-    return render(request, 'index.html', {'Books': Books})
+    Books = Book.objects.all().order_by('-rating')
+    nBooks = Books.count()
+    avgRating = Books.aggregate(Avg('rating'))
+    return render(request, 'index.html', {'Books': Books, 'nBooks': nBooks, 'avgRating': avgRating})
 
-def detail(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
+def detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'detail.html', {'book': book})
