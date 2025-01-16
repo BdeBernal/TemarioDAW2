e799b0cd139450eb75f3460f51aca45b3dbb9b6e
Bernal <=>
2025-01-16 13:43:40 +0100

    Update BookAdmin in admin.py

--- Temario/DWCS/django2/book_store/book_outlet/admin.py
+++ Temario/DWCS/django2/book_store/book_outlet/admin.py
@@ -5,5 +5,6 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ('rating', 'author') 
+    list_display = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
