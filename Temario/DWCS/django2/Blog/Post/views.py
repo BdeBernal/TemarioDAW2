851da505f1aa9dc547c69752d06529fee8c3898c
Bernal <=>
2025-01-22 12:56:36 +0100

    wordpress y django

--- Temario/DWCS/django2/Blog/Post/views.py
+++ Temario/DWCS/django2/Blog/Post/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 from .models import Post
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-date')
     return render(request, 'index.html', {'posts': posts})
+
+def detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'detail.html', {'post': post})
+
+def all(request):
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'all.html', {'posts': posts})
--- Temario/DWCS/django2/Blog/administracion/urls.py
+++ Temario/DWCS/django2/Blog/administracion/urls.py
@@ -21,4 +21,5 @@ from Post import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('<slug:slug>', views.detail, name='detail'),
 ]
