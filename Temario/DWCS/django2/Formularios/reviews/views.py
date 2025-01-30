d558ad520a63b8ec243cec08cc5dde4dbf3161f6
Bernal <=>
2025-01-30 14:29:22 +0100

    sessions otra cosa

--- Temario/DWCS/django2/Formularios/reviews/views.py
+++ Temario/DWCS/django2/Formularios/reviews/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
 
@@ -10,18 +10,29 @@ def review(request):
 
         if form.is_valid():
             review = Review(
-                username=form.cleaned_data['username'], 
+                username=form.cleaned_data['username'],
                 password=form.cleaned_data['password'],
                 city=form.cleaned_data['city'],
                 webserver=form.cleaned_data['webserver'],
                 role=form.cleaned_data['role']
             )
+
             review.save()
-            return HttpResponseRedirect('/thank-you', {'review': review})
+            
+            # Store data in session
+            request.session['review_data'] = {
+                'username': form.cleaned_data['username'],
+                'city': form.cleaned_data['city'],
+                'webserver': form.cleaned_data['webserver'],
+                'role': form.cleaned_data['role']
+            }
+            
+            return HttpResponseRedirect('/thank-you')
     else:
         form = ReviewForm()   
 
     return render(request, 'review.html', {'form': form})
 
 def thank_you(request):
-    return render(request, 'thank_you.html')
+    review_data = request.session.get('review_data')
+    return render(request, 'thank_you.html', {'review_data': review_data})
