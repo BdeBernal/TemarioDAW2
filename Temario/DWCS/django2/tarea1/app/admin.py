a92dc42727186331b9abaa1b4622d86c0aa47b75
Bernal <a23BernalRC@iessanclemente.net>
2025-03-07 11:39:35 +0100

    tarea acabada

--- Temario/DWCS/django2/tarea1/app/admin.py
+++ Temario/DWCS/django2/tarea1/app/admin.py
@@ -3,6 +3,8 @@ from .models import Student, Degree
 # Register your models here.
 class StudentAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("surname", ) }
+    list_display = ("name", "surname", "degree")
+    list_filter = ("name", "surname")
 
 admin.site.register(Student,StudentAdmin)
 admin.site.register(Degree)
